55d6446deb93a907dad4e047f7bbcc8687771a28
Bogna <wrobel.bogna@gmail.com>
2017-03-31 08:43:27 +0200

    ordered

--- game.js
+++ game.js
@@ -1,101 +1,66 @@
-function startGame(symbol) {
-    document.symbol = symbol;
-    // to be able to reset game with starting symbol
-    starting = symbol;
-    winner = false;
-    moveCounter = 0;
-    clear.clearBoard();
-}
-
-function resetGame() {
-    startGame(starting);
-}
-
-function nextMove(cell) {
-    if (cell.innerText == '' && winner == false && moveCounter != 9) {
-        cell.innerText = document.symbol;
-        if (isX(cell)) {
-            cell.style.color = "#191970";
-        }
-        if (isO(cell)) {
-            cell.style.color = "red";
-        }
-
-        switchSymbol();
-        moveCounter++;
-        if (winnerChecker.isDraw(moveCounter) && winner == false) {
-            historyModule.wasDraw();
-             setDraw(historyModule.getTimesWasDraw());
-            alert('Game over. It is a tie.');
-        }
+var gameModule = (function () {
+    var _startGame = function (symbol) {
+        document.symbol = symbol;
+        // to be able to reset game with starting symbol
+        starting = symbol;
+        winner = false;
+        moveCounter = 0;
+        clear.clearBoard();
     }
-    else if (cell.innerText == '' && winner == true) {
-        alert("Game over,  you cannot place symbol on board.");
-    }
-    else {
-        setMessage('This field is occupied. Pick another.');
-    }
-}
-function switchSymbol() {
-    if (winnerChecker.isWon(document.symbol)) {
-        winner = true;
-        if(isSymbolX()){
-            historyModule.xWon();
-            setXWin(historyModule.getTimesXWon());
-        }
-        else{
-            historyModule.oWon();
-            setOWin(historyModule.getTimesOWon());
-        }
-        alert(document.symbol + ', you won!');
-    }
+    _resetGame = function () {
+        _startGame(starting);
+    },
+        _switchSymbol = function () {
+            if (winnerChecker.isWon(document.symbol)) {
+                winner = true;
+                if (checkSymbolModule.isSymbolX()) {
+                    historyModule.xWon();
+                    messagesModule.setXWin(historyModule.getTimesXWon());
+                }
+                else {
+                    historyModule.oWon();
+                    messagesModule.setOWin(historyModule.getTimesOWon());
+                }
+                alert(document.symbol + ', you won!');
+            }
 
-    else if (document.symbol == 'X') {
-        document.symbol = 'O';
-        setMessage(document.symbol + ' turn');
+            else if (document.symbol == 'X') {
+                document.symbol = 'O';
+                messagesModule.setMessage(document.symbol + ' turn');
+            }
+            else {
+                document.symbol = 'X';
+                messagesModule.setMessage(document.symbol + ' turn');
+            }
+        },
+        _nextMove = function (cell) {
+            if (cell.innerText == '' && winner == false && moveCounter != 9) {
+                cell.innerText = document.symbol;
+                if (checkSymbolModule.isX(cell)) {
+                    cell.style.color = "#191970";
+                }
+                if (checkSymbolModule.isO(cell)) {
+                    cell.style.color = "red";
+                }
+
+                _switchSymbol();
+                moveCounter++;
+                if (winnerChecker.isDraw(moveCounter) && winner == false) {
+                    historyModule.wasDraw();
+                    messagesModule.setDraw(historyModule.getTimesWasDraw());
+                    alert('Game over. It is a tie.');
+                }
+            }
+            else if (cell.innerText == '' && winner == true) {
+                alert("Game over,  you cannot place symbol on board.");
+            }
+            else {
+                messagesModule.setMessage('This field is occupied. Pick another.');
+            }
+        };
+    return {
+        startGame: _startGame,
+        resetGame: _resetGame,
+        nextMove: _nextMove
     }
-    else {
-        document.symbol = 'X';
-        setMessage(document.symbol + ' turn');
-    }
-}
-
-function setMessage(msg) {
-    document.getElementById('msg').innerText = msg;
-}
-
-function setDraw(msg) {
-    document.getElementById('draw').innerText = msg;
-}
-
-function setXWin(msg) {
-    document.getElementById('xWin').innerText = msg;
-}
-
-function setOWin(msg) {
-    document.getElementById('oWin').innerText = msg;
-}
-
-function isX(cell){
-    var isX=false;
-    if(cell.innerText=='X'){
-       isX=true;
-    }
-    return isX;
-}
-
-function isO(cell){
-    var isO=false;
-    if(cell.innerText=='O'){
-       isO=true;
-    }
-    return isO;
-}
-
-function isSymbolX(){
-    var isSymbolX=false;
-    if(document.symbol=='X'){
-        isSymbolX=true;
-    }
-    return isSymbolX;
-}
+})();
--- winner-checker.js
+++ winner-checker.js
@@ -1,30 +1,31 @@
-
-
-function isWon(symbol) {
-    var result = false;
-    if (checkRow(1, 2, 3, symbol) || checkRow(4, 5, 6, symbol) || checkRow(7, 8, 9, symbol) || checkRow(1, 4, 7, symbol) || checkRow(2, 5, 8, symbol) || checkRow(3, 6, 9, symbol) || checkRow(1, 5, 9, symbol) || checkRow(3, 5, 7, symbol)) {
-        result = true;
-    }
-    return result;
-}
-
-function checkRow(field1, field2, field3, symbol) {
-    var isRowTaken = false;
-    var allTakenInRow = (getSymbol(field1) == symbol && getSymbol(field2) == symbol && getSymbol(field3) == symbol);
-    if (allTakenInRow) {
-        isRowTaken = true;
-    }
-    return isRowTaken;
-}
-
-function getSymbol(fieldId) {
-    return document.getElementById(fieldId).innerText;
-}
-
-function isDraw(moveCounter) {
-    var draw = false;
-    if (moveCounter == 9) {
-        draw = true;
-    }
-    return draw;
-}
+var winnerChecker = (function () {
+    var _getSymbol = function (fieldId) {
+        return document.getElementById(fieldId).innerText;
+    },
+        _checkRow = function (field1, field2, field3, symbol) {
+            var isRowTaken = false;
+            var allTakenInRow = (_getSymbol(field1) == symbol && _getSymbol(field2) == symbol && _getSymbol(field3) == symbol);
+            if (allTakenInRow) {
+                isRowTaken = true;
+            }
+            return isRowTaken;
+        },
+        _isWon = function (symbol) {
+            var result = false;
+            if (_checkRow(1, 2, 3, symbol) || _checkRow(4, 5, 6, symbol) || _checkRow(7, 8, 9, symbol) || _checkRow(1, 4, 7, symbol) || _checkRow(2, 5, 8, symbol) || _checkRow(3, 6, 9, symbol) || _checkRow(1, 5, 9, symbol) || _checkRow(3, 5, 7, symbol)) {
+                result = true;
+            }
+            return result;
+        },
+        _isDraw = function (moveCounter) {
+            var draw = false;
+            if (moveCounter == 9) {
+                draw = true;
+            }
+            return draw;
+        };
+    return {
+        isWon: _isWon,
+        isDraw: _isDraw
+    };
+})();
