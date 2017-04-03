f0f9e568061d01e493b0d8258667432857701d92
Bogna <wrobel.bogna@gmail.com>
2017-04-03 10:26:35 +0200

    with clear test

---
+++ test/clearSpec.js
@@ -0,0 +1,10 @@
+describe('board clearing',function(){
+    it('clears board', function(){
+        var field=document.createElement("id"),
+        empty='',nonempty='something';
+        field.innerText=nonempty;
+       document.getElementById= jasmine.createSpy('HTML Element').and.returnValue(field);
+        clear.clearBoard();
+        expect(field.innerText).toBe(empty);
+    });
+});
